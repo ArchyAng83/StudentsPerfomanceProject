8457f45a9a24c68d9310805e27ffb23930d395f3
saltkeeper1 <saltkeeper1@gmail.com>
2022-10-18 03:39:24 +0300

    Added models entity

---
+++ StudentsPerfomanceBL/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentsPerfomanceBL.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Adress { get; set; }        
+        public SchoolClass SchoolClass { get; set; }
+        public List<Mark> Marks { get; set; }
+        public List<Guardian> Guardians { get; set; }
+    }
+}
